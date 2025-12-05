487c0d9e445ad29955912cd5c08437fbdf9bcb27
Mahdi <msb585920@gmail.com>
2025-12-05 22:53:35 +0330

    a test jwt token created

--- jwt-create-token.php
+++ jwt-create-token.php
@@ -1,3 +1,25 @@
 <?php
-    
-?>
+
+include_once "vendor/autoload.php";
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$key = 'mahdi_sadeq_kdashhaskdfldjfsa';
+$payload = [
+    'iss' => 'http://example.org',
+    'aud' => 'http://example.com',
+    'iat' => 1356999524,
+    'nbf' => 1357000000,
+    'user_id' => 7
+];
+
+/**
+ * IMPORTANT:
+ * You must specify supported algorithms for your application. See
+ * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
+ * for a list of spec-compliant algorithms.
+ */
+$jwt = JWT::encode($payload, $key, 'HS256');
+
+echo $jwt;
